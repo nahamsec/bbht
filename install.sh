7b637e540653cfb0acb88a7e771cb92baddcbe79
inject <66717990+flag007@users.noreply.github.com>
2021-01-20 21:11:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,8 +38,8 @@ select choice in "${choices[@]}"; do
                 yes)
 
 					echo "Installing Golang"
-					wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
-					sudo tar -xvf go1.13.4.linux-amd64.tar.gz
+					wget https://golang.org/dl/go1.14.14.linux-amd64.tar.gz
+					sudo tar -xvf go1.14.14.linux-amd64.tar.gz
 					sudo mv go /usr/local
 					export GOROOT=/usr/local/go
 					export GOPATH=$HOME/go
